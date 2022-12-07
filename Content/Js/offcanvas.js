2b601c4c484dd10d67e8e84216f389053d2f7542
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2022-12-07 00:45:37 +0000

    Added nav brand, black background for navigation, nav links on click close the offcanvas.

--- Content/Js/offcanvas.js
+++ Content/Js/offcanvas.js
@@ -1,10 +1,20 @@
 const openbtn = document.getElementById('open');
 const closebtn = document.getElementById('close');
 const offcanvasMenu = document.getElementById('menu');
+const menuLink = document.getElementsByClassName('menu-link');
+const background = document.getElementById('background');
 
-openbtn.addEventListener('click', function showOffcanvas() {
+openbtn.addEventListener('click', function showOffcanvas(e) {
+    e.preventDefault();
     offcanvasMenu.classList.add('offcanvas-menu-active');
+    background.classList.add('black-bg');
 });
-closebtn.addEventListener('click', function hideOffcanvas() {
+closebtn.addEventListener('click', function hideOffcanvas(e) {
+    e.preventDefault();
     offcanvasMenu.classList.remove('offcanvas-menu-active');
+    background.classList.remove('black-bg');
 });
+function hideOffcanvas() {
+    offcanvasMenu.classList.remove('offcanvas-menu-active');
+    background.classList.remove('black-bg');
+};
